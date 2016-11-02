4225b4fc8f2a826471c7558c17f3685a1146b1be
wws <798875038@qq.com>
2016-11-02 19:21:15 +0800

    test 什么都没有

--- app/src/main/java/com/product/yuwei/fragment/MyFragment.java
+++ app/src/main/java/com/product/yuwei/fragment/MyFragment.java
@@ -1,7 +1,29 @@
 package com.product.yuwei.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.product.yuwei.R;
+import com.product.yuwei.bean.BaseFragment;
+
+import java.util.zip.Inflater;
+
 /**
  * Created by Administrator on 2016/10/17 0017.
  */
-public class MyFragment {
+public class MyFragment extends BaseFragment{
+    @Override
+    public String getFragmentTitle() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.My)
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
