2d81303a346450f0c9feee359071c3c371b64316
tengye <843203996@qq.com>
2016-11-23 19:48:30 +0800

    marge text

--- app/src/main/java/com/product/yuwei/YuweiApplication.java
+++ app/src/main/java/com/product/yuwei/YuweiApplication.java
@@ -37,11 +37,11 @@ public class YuweiApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //SDKInitializer.initialize(getApplicationContext());
-        //初始化
-        //x.Ext.init(this);
-        //设置是否输出debug
-        //x.Ext.setDebug(BuildConfig.DEBUG);
+        SDKInitializer.initialize(getApplicationContext());
+//        初始化
+        x.Ext.init(this);
+//        设置是否输出debug
+        x.Ext.setDebug(BuildConfig.DEBUG);
 
 
         try {
