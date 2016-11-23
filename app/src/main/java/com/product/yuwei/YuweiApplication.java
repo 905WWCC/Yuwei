26a8c2aba0dfac88796f5d545431ac6866ea4a1b
Chenlibin <913342457@qq.com>
2016-11-23 19:44:04 +0800

    Update YuweiApplication.java

--- app/src/main/java/com/product/yuwei/YuweiApplication.java
+++ app/src/main/java/com/product/yuwei/YuweiApplication.java
@@ -37,11 +37,11 @@ public class YuweiApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SDKInitializer.initialize(getApplicationContext());
+        //SDKInitializer.initialize(getApplicationContext());
         //初始化
-        x.Ext.init(this);
+        //x.Ext.init(this);
         //设置是否输出debug
-        x.Ext.setDebug(BuildConfig.DEBUG);
+        //x.Ext.setDebug(BuildConfig.DEBUG);
 
 
         try {
